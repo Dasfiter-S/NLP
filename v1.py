c18b2fceeeffd114c6cd9630ad75ec520c01464d
Ricardo Carretero <39317596+Dasfiter-S@users.noreply.github.com>
2019-10-03 15:29:42 -0700

    Add files via upload

--- v1.py
+++ v1.py
@@ -1,23 +1,25 @@
 #import tensorflow as tf
 import argparse
 import json
+import 
 
 #commandline options
 def launch_options():
     parser = argparse.ArgumentParser("This program analyzes synonyms and returns the most\
                                   likely candidates that are similar to the word provided.")
-    parser.add_argument('-s', '--synonym', help='Choose a word to analyze.')
+    parser.add_argument('-s', '--synonym', help='Choose a word to analyze.', type=str)
     parser.add_argument('-f', '--file', help='You can load your own dictionary. Will have \
-                        to recompute the tables however.')
+                        to recompute the tables however.', type=str)
+    parser.add_argument('-r', '--recompute', help='Load default data file and compute\
+                        again.', default=False)
     arg = parser.parse_args()
-    if arg.synonym is None:
+    if arg.synonym is None and arg.recompute:
         print("Nothing to do. No word given.")
         exit()
     return arg
 
 
 class no_name(object):
-    
     def load_file(file_in):
         with open(file_in, 'r') as f:
             info = json.loads(f)
@@ -27,8 +29,11 @@ class no_name(object):
 if __name__ == "__main__":
     arg_options = launch_options()
     json_data = ""
+    default_data = "DA.json"
     if arg_options.file is not None:
         json_data = test_item.load_file(file_in);
+    if arg_options.recompute:
+        json_data = test_item.load_file(default_data);
 
     #Not always needed, only needed when recomputing the tables
     #processed_data = find_connections(data);
